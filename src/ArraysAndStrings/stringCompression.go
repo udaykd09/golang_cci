69dfef9bbefe80ed349e238c31cc12676c47325c
Uday Dungarwal <udaykd09@gmail.com>
2018-01-09 16:01:55 -0800

    Adding LinkedList code

--- src/ArraysAndStrings/stringCompression.go
+++ src/ArraysAndStrings/stringCompression.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"strings"
+	"strconv"
 )
 
-func compress(s string) string {
-	x = strings.ToLower(s)
-	count = 0
+func compress(x string) string {
+	s := strings.ToLower(x)
+	compressedStr := ""
+	count := 0
 	for i:=0; i<len(s); i++ {
-		i
+		count++
+		if i+1>=len(s) || s[i] != s[i+1] {
+			compressedStr += string(s[i]) + strconv.Itoa(count)
+			count = 0
+		}
+	}
+	if len(compressedStr) < len(s) {
+		return compressedStr
+	} else {
+		return x
 	}
 }
 
 func main() {
-	
+	fmt.Println(compress("acd"))
 }
 
